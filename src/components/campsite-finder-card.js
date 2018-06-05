1f43538ccbdfc54e5845562905f58efd2e8f56d2
John Gibbons <jgibbons@linkedin.com>
2018-06-04 23:10:33 -0700

    update card to show correct image url

--- src/components/campsite-finder-card.js
+++ src/components/campsite-finder-card.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import './campsite-finder-card.css'
-import ResultsTable from './results-table'
-import CampsiteFinderSettingsForm from './campsite-finder-settings-form'
+import React from "react";
+import "./campsite-finder-card.css";
+import ResultsTable from "./results-table";
+import CampsiteFinderSettingsForm from "./campsite-finder-settings-form";
 import {
   Button,
   Card,
@@ -10,11 +10,11 @@ import {
   Icon,
   Dropdown,
   Confirm
-} from 'semantic-ui-react'
-import 'react-dates/lib/css/_datepicker.css'
-import { SPECIFIC_DATES } from '../modules/campsiteFinders'
-import { captializeTitle, replaceImages } from '../helpers/reducerHelpers'
-import moment from 'moment'
+} from "semantic-ui-react";
+import "react-dates/lib/css/_datepicker.css";
+import { SPECIFIC_DATES } from "../modules/campsiteFinders";
+import { captializeTitle, replaceImages } from "../helpers/reducerHelpers";
+import moment from "moment";
 
 const CampsiteFinderCard = ({
   campsiteFinder,
@@ -40,28 +40,30 @@ const CampsiteFinderCard = ({
     isShowingAllResults,
     isSettingsShowing,
     lastCheckedAt
-  } = campsiteFinder
-  const hasAvailableSites = datesAvailable.length > 0
+  } = campsiteFinder;
+  const hasAvailableSites = datesAvailable.length > 0;
+  console.log(campgroundId);
   return (
     <Card
-      className='campsite-finder-card'
-      color={hasAvailableSites ? 'green' : isActive ? 'red' : 'yellow'}
+      className="campsite-finder-card"
+      color={hasAvailableSites ? "green" : isActive ? "red" : "yellow"}
     >
       <Card.Content extra>
-        <Card.Header className='campsite-finder-card__header'>
+        <Card.Header className="campsite-finder-card__header">
           {captializeTitle(campgroundId.facilityName)}
-          <div className='campsite-finder-card__on-off'>
-            <Dropdown icon={isActive ? 'pause' : 'play'}>
+          <div className="campsite-finder-card__on-off">
+            <Dropdown icon={isActive ? "pause" : "play"}>
               <Dropdown.Menu>
                 <Dropdown.Item
                   onClick={() =>
-                    handleUpdateCampsiteFinder(_id, { isActive: !isActive })}
+                    handleUpdateCampsiteFinder(_id, { isActive: !isActive })
+                  }
                 >
-                  <Icon name={isActive ? 'pause' : 'play'} fitted />
-                  {isActive ? 'Pause' : 'Resume'}
+                  <Icon name={isActive ? "pause" : "play"} fitted />
+                  {isActive ? "Pause" : "Resume"}
                 </Dropdown.Item>
                 <Dropdown.Item onClick={() => handleToggleConfirm(_id)}>
-                  <Icon name='delete' />
+                  <Icon name="delete" />
                   Delete
                 </Dropdown.Item>
                 <Confirm
@@ -74,98 +76,99 @@ const CampsiteFinderCard = ({
           </div>
         </Card.Header>
       </Card.Content>
-      <div className='campsite-finder-card__image-wrapper'>
+      <div className="campsite-finder-card__image-wrapper">
         <a
-          className='campsite-finder-card__title-link'
+          className="campsite-finder-card__title-link"
           href={campgroundId.url}
-          target='blank'
+          target="blank"
         >
           <Image
-            className='campsite-finder-card__campground-image'
-            src={replaceImages(
-              `http://reserveamerica.com${campgroundId.facilityPhoto}`
-            )}
+            className="campsite-finder-card__campground-image"
+            src={replaceImages(campgroundId.facilityPhoto)}
           />
         </a>
       </div>
-      <Card.Content className='campsite-finder-card__settings'>
-        <Card.Meta className='campsite-finder-card__sub-heading'>
+      <Card.Content className="campsite-finder-card__settings">
+        <Card.Meta className="campsite-finder-card__sub-heading">
           Settings
-          {isSettingsShowing
-            ? <div className='campsite-finder-card__edit-link'>
+          {isSettingsShowing ? (
+            <div className="campsite-finder-card__edit-link">
               <Button onClick={() => handleCancelEditSettings(_id)} compact>
-                  Cancel
+                Cancel
               </Button>
               <Button
-                onClick={() =>
-                  handleUpdateCampsiteFinder(_id, campsiteFinder)}
+                onClick={() => handleUpdateCampsiteFinder(_id, campsiteFinder)}
                 primary
                 compact
               >
-                  Save
+                Save
               </Button>
             </div>
-            : <a
-              className='campsite-finder-card__edit-link'
+          ) : (
+            <a
+              className="campsite-finder-card__edit-link"
               onClick={() => handleToggleSettigsFormShowing(_id)}
             >
-                Edit
-            </a>}
+              Edit
+            </a>
+          )}
         </Card.Meta>
-        {isSettingsShowing
-          ? <CampsiteFinderSettingsForm campsiteFinder={campsiteFinder} />
-          : <List>
+        {isSettingsShowing ? (
+          <CampsiteFinderSettingsForm campsiteFinder={campsiteFinder} />
+        ) : (
+          <List>
             <List.Item>
-              <List.Header className='float-left'>Dates</List.Header>
-              <span className='float-right'>
+              <List.Header className="float-left">Dates</List.Header>
+              <span className="float-right">
                 {dateOption === SPECIFIC_DATES
-                  ? `${moment(startDate).format('MM/DD/YY')} -
-                    ${moment(endDate).format('MM/DD/YY')}`
-                  : 'Next Six Months'}
+                  ? `${moment(startDate).format("MM/DD/YY")} -
+                    ${moment(endDate).format("MM/DD/YY")}`
+                  : "Next Six Months"}
               </span>
             </List.Item>
-            {isWeekendsOnly &&
-                <List.Item>
-                  <List.Header className='float-left'>
-                    Weekends Only
-                  </List.Header>
-                </List.Item>}
-            {siteCode &&
-                <List.Item>
-                  <List.Header className='float-left'>Site Codes</List.Header>
-                  <span className='float-right'>
-                    {siteCode}
-                  </span>
-                </List.Item>}
+            {isWeekendsOnly && (
+              <List.Item>
+                <List.Header className="float-left">Weekends Only</List.Header>
+              </List.Item>
+            )}
+            {siteCode && (
+              <List.Item>
+                <List.Header className="float-left">Site Codes</List.Header>
+                <span className="float-right">{siteCode}</span>
+              </List.Item>
+            )}
             <List.Item>
-              <List.Header className='float-left'>
-                  Notification Settings
+              <List.Header className="float-left">
+                Notification Settings
               </List.Header>
-              <span className='float-right'>
-                {isSendingEmails ? 'Email' : 'None'}
+              <span className="float-right">
+                {isSendingEmails ? "Email" : "None"}
               </span>
             </List.Item>
-          </List>}
+          </List>
+        )}
       </Card.Content>
       <Card.Content>
-        <Card.Meta className='campsite-finder-card__sub-heading'>
+        <Card.Meta className="campsite-finder-card__sub-heading">
           Results
-          <div className='campsite-finder-card__sub-sub-heading'>
+          <div className="campsite-finder-card__sub-sub-heading">
             {lastCheckedAt
               ? `Updated ${moment(lastCheckedAt).fromNow()}`
               : `Not checked yet`}
           </div>
         </Card.Meta>
-        {datesAvailable.length > 0
-          ? <ResultsTable
+        {datesAvailable.length > 0 ? (
+          <ResultsTable
             results={datesAvailable}
             isShowingAll={isShowingAllResults}
             handleToggleShowAll={() => handleToggleShowAllResults(_id)}
           />
-          : <div>No spots available</div>}
+        ) : (
+          <div>No spots available</div>
+        )}
       </Card.Content>
     </Card>
-  )
-}
+  );
+};
 
-export default CampsiteFinderCard
+export default CampsiteFinderCard;
