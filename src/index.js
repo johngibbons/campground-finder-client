445d0e2c1fbae5daa88ec38355bb5ab4ce510cd9
John Gibbons <jgibbons@linkedin.com>
2019-07-07 14:04:12 -0700

    use craco for semantic-ui theming

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
 import store, { history } from "./store";
-import "./semantic/dist/semantic.min.css";
+import 'semantic-ui-less/semantic.less'
 
 ReactDOM.render(
   <Provider store={store}>
